054fe405e8d21bc38107fb93836d09e1abbc4744
dianyuluo <1739556970@qq.com>
2022-08-08 23:38:50 +0800

    测试样例的基础使用方法

---
+++ test/test_main.py
@@ -0,0 +1,19 @@
+
+# from script.countwordfrequency.countword import
+import pytest
+
+
+class TestOneFLOW:
+    def test_01(self):
+        print("nice")
+
+    def test_02(self):
+        print('国产CFD开源软件OneFLOW自动测试-02')
+
+    def test_03(self):
+        print('国产CFD开源软件OneFLOW自动测试-03')
+
+
+class TestDB:
+    def test_db1(self):
+        print("china")
